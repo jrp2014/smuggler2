fe1bff8e7f9e82ee3bf8223e58260a138454505f
jrp2014 <jrp2014@users.noreply.github.com>
2020-05-16 16:34:54 +0100

    Handle the .import files that smuggler generates better
    so that there is less chance that different test instances will conflict

--- test/Test.hs
+++ test/Test.hs
@@ -8,24 +8,32 @@ import Smuggler.Options
     ImportAction (..),
     Options (..),
   )
-import System.FilePath ((-<.>), takeBaseName)
+import System.FilePath ((-<.>), (</>), takeBaseName)
 import System.Process.Typed (ProcessConfig, proc, runProcess_)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.Golden (findByExtension, goldenVsFileDiff)
+
+
+-- | Where the tests are, relative to the project level cabal file
+testDir :: FilePath
+testDir = "test" </> "tests"
 
 -- | Combinations of import and export action options to be tested
 optionsList :: [Options]
 optionsList =
   [ mkOptions PreserveInstanceImports NoExportProcessing,
+    mkOptions MinimiseImports ReplaceExports,
     mkOptions MinimiseImports NoExportProcessing,
     mkOptions NoImportProcessing AddExplicitExports,
-    mkOptions NoImportProcessing ReplaceExports,
-    mkOptions MinimiseImports ReplaceExports,
-    mkOptions NoImportProcessing NoExportProcessing
+    mkOptions NoImportProcessing NoExportProcessing,
+    mkOptions NoImportProcessing ReplaceExports
   ]
   where
     mkOptions :: ImportAction -> ExportAction -> Options
-    mkOptions i e = Options i e (Just (show i ++ show e))
+    mkOptions ia ea = Options ia ea (Just $ mkExt ia ea)
+
+mkExt :: ImportAction -> ExportAction -> String
+mkExt ia ea = show ia ++ show ea
 
 testOptions :: [Options] -> IO TestTree
 testOptions opts =
@@ -33,19 +41,20 @@ testOptions opts =
 
 goldenTests :: Options -> IO TestTree
 goldenTests opts = do
-  testFiles <- findByExtension [".hs"] "test/tests"
+  testFiles <- findByExtension [".hs"] testDir
   return $
     testGroup
-      (fromMaybe "NoNewExtension" (newExtension opts))
+      testName
       [ goldenVsFileDiff
           (takeBaseName testFile) -- test name
-          (\ref new -> ["git", "diff", ref, new]) -- how to display diffs
-            -- (\ref new -> ["diff", "-u", ref, new]) -- how to display diffs
-          (testFile -<.> fromMaybe "golden" (newExtension opts) ++ "-golden") -- golden file
-          (testFile -<.> fromMaybe "NoNewExtension" (newExtension opts)) -- output file
+          (\ref new -> ["diff", "-u", ref, new]) -- how to display diffs
+          (testFile -<.> testName  ++ "-golden") -- golden file
+          (testFile -<.> testName) -- output file
           (compile testFile opts)
         | testFile <- testFiles
       ]
+  where
+      testName = fromMaybe "NoNewExtension" (newExtension opts)
 
 main :: IO ()
 main = defaultMain =<< testOptions optionsList
@@ -63,14 +72,18 @@ compile testcase opts = runProcess_ cabalConfig
 
     cabalArgs :: [String]
     cabalArgs =
-      -- no sure why it is necessary to mention the smuggler package explictly,
-      -- but it appears to be hidden otherwise
-      ["exec", "--", "ghc", "-package smuggler", "-fno-code", "-fplugin=Smuggler.Plugin"]
+      -- * no sure why it is necessary to mention the smuggler package explictly,
+      -- but it appears to be hidden otherwise.
+      -- * This puts the .imports files that smuggler generates somewhere they
+      -- can easily be found
+      ["exec", "--", "ghc", "-dumpdir=" ++ testDir, "-package smuggler", "-fno-code", "-fplugin=Smuggler.Plugin"]
         ++ map
           ("-fplugin-opt=Smuggler.Plugin:" ++)
-          ( let p = [show (importAction opts), show (exportAction opts)]
+          ( let ia = importAction opts
+                ea = exportAction opts
+                p = [show ia, show ea]
              in case newExtension opts of
-                  Nothing -> p
+                  Nothing -> mkExt ia ea : p -- provide a default extension
                   Just e -> e : p
           )
         ++ [testcase]
